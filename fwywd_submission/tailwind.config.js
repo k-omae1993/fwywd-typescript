ce51eacc777d43a6a7a69a3fe85695dadd89fe46
ko-datascience <ss-g1@ezweb.ne.jp>
2021-12-13 19:32:56 +0900

    create layout

--- fwywd_submission/tailwind.config.js
+++ fwywd_submission/tailwind.config.js
@@ -2,6 +2,10 @@ module.exports = {
   mode: 'jit',
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   theme: {
+    screens: {
+      'mobile': {'max': '415px'},
+      'desktop': {'min': '416px', },
+    },
     extend: {},
   },
   plugins: [],
